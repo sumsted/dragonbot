b708d5dfa37c3436d17470187cc2ed1f93417f0d
Scott Umsted <scottumsted@gmail.com>
2016-04-01 12:30:30 -0500

    create robot object

--- api/Robot_server.py
+++ api/Robot_server.py
@@ -24,56 +24,56 @@ def handle_padded(handler):
 @get('/Robot___init__/<addr>/<left_id>/<right_id>/<left_trim>/<right_trim>/<stop_at_exit>')
 @handle_padded
 def Robot___init__(kargs):
-    r = {'return_value': robot.Robot___init__(int(kargs['addr']), int(kargs['left_id']), int(kargs['right_id']), int(kargs['left_trim']), int(kargs['right_trim']), int(kargs['stop_at_exit']))}
+    r = {'return_value': robot.__init__(int(kargs['addr']), int(kargs['left_id']), int(kargs['right_id']), int(kargs['left_trim']), int(kargs['right_trim']), int(kargs['stop_at_exit']))}
     return r
 
 
 @get('/Robot__left_speed/<speed>')
 @handle_padded
 def Robot__left_speed(kargs):
-    r = {'return_value': robot.Robot__left_speed(int(kargs['speed']))}
+    r = {'return_value': robot._left_speed(int(kargs['speed']))}
     return r
 
 
 @get('/Robot__right_speed/<speed>')
 @handle_padded
 def Robot__right_speed(kargs):
-    r = {'return_value': robot.Robot__right_speed(int(kargs['speed']))}
+    r = {'return_value': robot._right_speed(int(kargs['speed']))}
     return r
 
 
 @get('/Robot_stop')
 @handle_padded
 def Robot_stop(kargs):
-    r = {'return_value': robot.Robot_stop()}
+    r = {'return_value': robot.stop()}
     return r
 
 
 @get('/Robot_forward/<speed>/<seconds>')
 @handle_padded
 def Robot_forward(kargs):
-    r = {'return_value': robot.Robot_forward(int(kargs['speed']), int(kargs['seconds']))}
+    r = {'return_value': robot.forward(int(kargs['speed']), int(kargs['seconds']))}
     return r
 
 
 @get('/Robot_backward/<speed>/<seconds>')
 @handle_padded
 def Robot_backward(kargs):
-    r = {'return_value': robot.Robot_backward(int(kargs['speed']), int(kargs['seconds']))}
+    r = {'return_value': robot.backward(int(kargs['speed']), int(kargs['seconds']))}
     return r
 
 
 @get('/Robot_right/<speed>/<seconds>')
 @handle_padded
 def Robot_right(kargs):
-    r = {'return_value': robot.Robot_right(int(kargs['speed']), int(kargs['seconds']))}
+    r = {'return_value': robot.right(int(kargs['speed']), int(kargs['seconds']))}
     return r
 
 
 @get('/Robot_left/<speed>/<seconds>')
 @handle_padded
 def Robot_left(kargs):
-    r = {'return_value': robot.Robot_left(int(kargs['speed']), int(kargs['seconds']))}
+    r = {'return_value': robot.left(int(kargs['speed']), int(kargs['seconds']))}
     return r
 
 
