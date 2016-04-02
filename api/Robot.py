c109a5e74127ad56d52021d7d374ecd5dfca3f43
Scott Umsted <scottumsted@gmail.com>
2016-04-02 13:22:09 -0500

    corrected glides

--- api/Robot.py
+++ api/Robot.py
@@ -124,10 +124,10 @@ class Robot(object):
         the glides are to provide real left and right
         '''
         # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(0)
-        self._left.run(Adafruit_MotorHAT.FORWARD)
-        self._right.run(Adafruit_MotorHAT.BACKWARD)
+        self._left_speed(0)
+        self._right_speed(speed)
+        self._left.run(Adafruit_MotorHAT.BACKWARD)
+        self._right.run(Adafruit_MotorHAT.FORWARD)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
@@ -138,10 +138,10 @@ class Robot(object):
         the glides are to provide real left and right
         '''
         # Set motor speed and move both forward.
-        self._left_speed(0)
-        self._right_speed(speed)
-        self._left.run(Adafruit_MotorHAT.BACKWARD)
-        self._right.run(Adafruit_MotorHAT.FORWARD)
+        self._left_speed(speed)
+        self._right_speed(0)
+        self._left.run(Adafruit_MotorHAT.FORWARD)
+        self._right.run(Adafruit_MotorHAT.BACKWARD)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
